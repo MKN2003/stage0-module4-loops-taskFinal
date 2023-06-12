0d0cc8e4f470ae6e9e6e269463fdfd5c02062e35
MKN2003 <kazimovmiraziz197@gmail.com>
2023-06-12 13:32:55 +0500

    changes have been made

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        int product = 0;
+        for (int i = 1; i <= 10; i++) {
+            product = numberTableToPrint * i;
+            System.out.println(i + " x " + numberTableToPrint + " = " + product);
+        }
     }
 }
